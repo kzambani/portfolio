ee44bd067a37f99e9911ea36daa1924f1fd7750a
kzambani <165939390+kzambani@users.noreply.github.com>
2025-01-24 20:14:09 -0800

    updated link generation

--- global.js
+++ global.js
@@ -17,25 +17,54 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+// for (let p of pages) {
+//   let url = p.url;
+//   let title = p.title;
+  
+//   url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  
+//   let a = document.createElement('a');
+//   a.href = url;
+//   a.textContent = title;
+  
+//   a.classList.toggle(
+//     'current',
+//     a.host === location.host && a.pathname === location.pathname
+//   );
+  
+//   if (a.host !== location.host) {
+//     a.target = "_blank";
+//   }
+  
+//   nav.append(a);
+// }
+
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  
-  url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-  
+
+  // URL generation
+  if (!ARE_WE_HOME) {
+      // if not on home page, use relative path
+      url = url.startsWith('http') ? url : `./${url}`;
+  } else {
+      // if on home page, use root-relative paths
+      url = url.startsWith('http') ? url : `/${url}`;
+  }
+
   let a = document.createElement('a');
   a.href = url;
   a.textContent = title;
-  
+
   a.classList.toggle(
     'current',
     a.host === location.host && a.pathname === location.pathname
   );
-  
+
   if (a.host !== location.host) {
     a.target = "_blank";
   }
-  
+
   nav.append(a);
 }
 
